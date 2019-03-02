0947c72e50de93c0ad540583ab2cbbf8bd7f67cf
nicetoseeyou <hello4world@yeah.net>
2019-03-02 19:26:04 +0800

    update data filtering run script

--- bin/start-data-filtering.sh
+++ bin/start-data-filtering.sh
@@ -3,4 +3,9 @@ baseDir=$(cd `dirname $0`; pwd)
 parentDir=$(cd `dirname $baseDir`; pwd)
 CLASS_PATH=${parentDir}/lib
 
-nohup java -Dlogback.configurationFile=${parentDir}/config/logback.xml -cp ${CLASS_PATH}/*:${CLASS_PATH}/${project.build.finalName}.jar fuck.hsbc.kafka.stream.filter.FilterApp </dev/null >/dev/null 2>&1 &
+leftInputTopic=$1
+rightInputTopic=$2
+rightStateStore=$3
+configFile=$4
+
+nohup java -Dlogback.configurationFile=${parentDir}/config/logback.xml -cp ${CLASS_PATH}/*:${CLASS_PATH}/${project.build.finalName}.jar fuck.hsbc.kafka.stream.filter.FilterApp ${leftInputTopic} ${rightInputTopic} ${rightStateStore} ${configFile} </dev/null >/dev/null 2>&1 &
