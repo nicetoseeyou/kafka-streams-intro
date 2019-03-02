5d2755af8af6aa3b0ca4346dd23073bb36bd1933
nicetoseeyou <hello4world@yeah.net>
2019-03-03 01:01:39 +0800

    fix key mapper logic in join

--- src/main/java/fuck/hsbc/kafka/stream/filter/DataFiltering.java
+++ src/main/java/fuck/hsbc/kafka/stream/filter/DataFiltering.java
@@ -8,11 +8,17 @@ import fuck.hsbc.kafka.stream.dto.JoinerKV;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.ValueJoiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -69,10 +75,14 @@ public class DataFiltering implements Filter {
         final StreamsBuilder builder = new StreamsBuilder();
         final KStream<JsonNode, JsonNode> left = builder.stream(leftInputTopic);
         final GlobalKTable<JsonNode, JsonNode> right = builder.globalTable(rightInputTopic, Materialized.as(rightStateStore));
-        left.map(keyEnrich)
-                .join(right, keyRebuild, valueEnrich)
-                .filter((k, v) -> filterCondition.contains(v.findValue(filterColumn).asText()))
-                .map(valueRebuild)
+        left
+                .join(right, alterLeftKey, joinValue)
+                .filter((k, v) -> {
+                    String cv = v.findValue(filterColumn).asText();
+                    LOGGER.info("Filter => {}={}", filterColumn, cv);
+                    return filterCondition.contains(cv);
+                })
+                .map(restoreRecord)
                 .to(leftOutputTopic);
         return builder.build();
     }
@@ -96,34 +106,28 @@ public class DataFiltering implements Filter {
      * Usage: filtering.right.keys.mapping=right.keyColumn_a|left.valueColumn_a,right.keyColumn_b|left.valueColumn_b
      * It means two mapping: right.keyColumn_a=left.valueColumn_a and right.keyColumn_b=left.valueColumn_b
      */
-    private KeyValueMapper<JsonNode, JsonNode, KeyValue<JsonNode, JsonNode>> keyEnrich = (k, v) -> {
+    private KeyValueMapper<JsonNode, JsonNode, JsonNode> alterLeftKey = (k, v) -> {
         if (rightKeys.equals(leftKeys)) {
-            return KeyValue.pair(k, v);
+            return k;
         } else {
-            if (leftKeys.containsAll(keyJoinerMap.values())) {
-                LOGGER.info("KeyEnrichScenario: right.key is a subset of left.key");
-            } else {
-                LOGGER.info("KeyEnrichScenario: right.key not a subset of left.key");
-            }
-            LOGGER.info("BeforeKeyEnrich: [{},{}]", k, v);
-            ObjectNode enrichedLeftKey = JsonNodeFactory.instance.objectNode();
-            keyJoinerMap.forEach((rightKeyColumn, leftColumn) -> enrichedLeftKey.set(rightKeyColumn, v.findValue(leftColumn)));
-            LOGGER.info("AfterKeyEnrich: [{},{}]", enrichedLeftKey, v);
-            return KeyValue.pair(enrichedLeftKey, v);
+            ObjectNode alteredLeftKey = JsonNodeFactory.instance.objectNode();
+            keyJoinerMap.forEach((rightKeyColumn, leftColumn) -> alteredLeftKey.set(rightKeyColumn, v.findValue(leftColumn)));
+            LOGGER.info("AfterAlterKey => left.key={}", alteredLeftKey);
+            return alteredLeftKey;
         }
     };
 
     /**
      * Value Join logic
      */
-    private ValueJoiner<JsonNode, JsonNode, JsonNode> valueEnrich = (lv, rv) -> {
+    private ValueJoiner<JsonNode, JsonNode, JsonNode> joinValue = (lv, rv) -> {
         ((ObjectNode) lv).set(filterColumn, rv.findValue(filterColumn));
         return lv;
     };
 
     /**
-     * Rebuild left.key after joining.
-     * Possible cases:
+     * Restore left record after joining
+     * left.key rebuild possible cases:
      * 1. right.key is a subset of left.value
      * 1.1 right.key = left.key, do nothing
      * 1.2 right.key != left.key
@@ -133,29 +137,16 @@ public class DataFiltering implements Filter {
      * <p>
      * Note: joiner must be the right keys
      */
-    private KeyValueMapper<JsonNode, JsonNode, JsonNode> keyRebuild = (k, v) -> {
-        if (rightKeys.equals(leftKeys)) {
-            return k;
-        } else {
-            if (leftKeys.containsAll(keyJoinerMap.values())) {
-                LOGGER.info("KeyRebuildScenario: right.key is a subset of left.key");
-            } else {
-                LOGGER.info("KeyRebuildScenario: right.key not a subset of left.key");
-            }
-            LOGGER.info("BeforeKeyRebuild: [{},{}]", k, v);
-            ObjectNode rebuiltLeftKey = JsonNodeFactory.instance.objectNode();
-            leftKeys.forEach(leftKeyColumn -> rebuiltLeftKey.set(leftKeyColumn, v.findValue(leftKeyColumn)));
-            LOGGER.info("AfterKeyRebuild: [{},{}]", rebuiltLeftKey, v);
-            return rebuiltLeftKey;
-        }
-    };
-
-    /**
-     * Value rebuild logic after joining
-     */
-    private KeyValueMapper<JsonNode, JsonNode, KeyValue<JsonNode, JsonNode>> valueRebuild = (k, v) -> {
+    private KeyValueMapper<JsonNode, JsonNode, KeyValue<JsonNode, JsonNode>> restoreRecord = (k, v) -> {
         ((ObjectNode) v).remove(filterColumn);
-        return KeyValue.pair(k, v);
+        if (rightKeys.equals(leftKeys)) {
+            return KeyValue.pair(k, v);
+        } else {
+            ObjectNode restoredLeftKey = JsonNodeFactory.instance.objectNode();
+            leftKeys.forEach(leftKeyColumn -> restoredLeftKey.set(leftKeyColumn, v.findValue(leftKeyColumn)));
+            LOGGER.info("AfterRecordRestore => left.key={}", restoredLeftKey);
+            return KeyValue.pair(restoredLeftKey, v);
+        }
     };
 
 }
